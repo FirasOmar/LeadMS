c248bc4ed7ca06992195c073824b85cf24a33122
Firas Omar <firas.thawabi@outlook.com>
2019-06-16 15:16:29 +0300

    update user profile resources

--- CRM.Web/App_GlobalResources/Account/UserProfile.Designer.cs
+++ CRM.Web/App_GlobalResources/Account/UserProfile.Designer.cs
@@ -19,7 +19,7 @@ namespace CRM.Web.App_GlobalResources.Account {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class UserProfile {
@@ -133,7 +133,7 @@ namespace CRM.Web.App_GlobalResources.Account {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to المحتوى باللغة العبرية.
+        ///   Looks up a localized string similar to المحتوى باللغة الانجليزية.
         /// </summary>
         public static string HebrewContent {
             get {
